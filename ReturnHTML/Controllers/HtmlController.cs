48e815215d001ca94cafd627b7ddc942d6364a13
burak <burak@ATHENA>
2022-11-27 11:52:14 +0300

    Add project files.

---
+++ ReturnHTML/Controllers/HtmlController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReturnHTML.Services;
+
+namespace ReturnHTML.Controllers
+{
+    [ApiController]
+    [Route("[action]")]
+    public class HtmlController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Index()
+        {
+            try
+            {
+                var html = "<p>Html Code</p>";
+
+                return base.Content(html, "text/html");
+            }
+            catch (Exception ex)
+            {
+                var response = new ServiceResponse();
+                response.Success = false;
+                response.Message = $"<p>{ex.Message} </p>";
+
+                return base.Content(response.Message, "text/html");
+            }
+
+        }
+        [HttpGet]
+        public ContentResult Verify()
+        {
+            try
+            {
+                var html = "<div> Your acc has benn verified! </div>";
+
+                return base.Content(html, "text/html");
+            }
+            catch (Exception ex)
+            {
+                var response = new ServiceResponse();
+                response.Success = false;
+                response.Message = $"<p>{ex.Message}</p>";
+
+                return base.Content(response.Message, "text/html");
+            }
+        }
+
+        [HttpGet]
+        public ContentResult ConfirmVerify()
+        {
+            try
+            {
+                var html = System.IO.File.ReadAllText(@"./Html/verified.html");
+
+                return base.Content(html, "text/html");
+
+            }
+            catch (Exception ex)
+            {
+                var response = new ServiceResponse();
+                response.Success = false;
+                response.Message = $"<p>{ex.Message}</p>";
+
+                return base.Content(response.Message, "text/html");
+            }
+        }
+
+
+        [HttpGet]
+        public ContentResult Welcome(string name)
+        {
+            try
+            {
+                var html = WelcomeService.WelcomeHTML(name);
+
+                return base.Content(html, "text/html");
+            }
+            catch (Exception ex)
+            {
+                var response = new ServiceResponse();
+                response.Success = false;
+                response.Message = $"<p>{ex.Message}</p>";
+
+                return base.Content(response.Message, "text/html");
+            }
+        }
+    }
+}
+        
+    
+
+    
+
+
---
+++ ReturnHTML/Services/WelcomeService.cs
@@ -0,0 +1,25 @@
+﻿namespace ReturnHTML.Services
+{
+    public class WelcomeService
+    {
+        public static string WelcomeHTML(string name)
+        {
+            try
+            {
+                var html = System.IO.File.ReadAllText(@"./Html/HomePage.html");
+
+                html = html.Replace("{{name}}", name);
+
+                return html;
+            }
+            catch (Exception ex)
+            {
+                var response = new ServiceResponse();
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return response.Success + "\n " + response.Message;
+            }
+        }
+    }
+}
